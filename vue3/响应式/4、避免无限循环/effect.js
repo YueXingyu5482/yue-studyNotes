5bf3eb944b73e7f31eb92437b95a7e61eee99d67
YueXingyu <yuexingyu@jixiang.cn>
2024-04-08 13:30:19 +0800

    添加备注

--- vue3/响应式/4、避免无限循环/effect.js
+++ vue3/响应式/4、避免无限循环/effect.js
@@ -1,58 +1,59 @@
-let activeEffect = null
-const effectStack = []
+let activeEffect = null;
+const effectStack = [];
 export function effect(fn) {
   const effectFn = () => {
-    cleanup(effectFn)
-    activeEffect = effectFn
-    effectStack.push(activeEffect)
-    fn()
-    effectStack.pop()
-    activeEffect = effectStack[effectStack.length - 1]
-  }
-  effectFn.deps = []
-  effectFn()
+    cleanup(effectFn);
+    activeEffect = effectFn;
+    effectStack.push(activeEffect);
+    fn();
+    effectStack.pop();
+    activeEffect = effectStack[effectStack.length - 1];
+  };
+  effectFn.deps = [];
+  effectFn();
 }
 function cleanup(effectFn) {
   for (let i = 0; i < effectFn.deps.length; i++) {
-    const dep = effectFn.deps[i]
-    dep.delete(effectFn)
+    const dep = effectFn.deps[i];
+    dep.delete(effectFn);
   }
-  effectFn.deps = []
+  effectFn.deps = [];
 }
-const bucket = new WeakMap()
+const bucket = new WeakMap();
 export function reactive(obj) {
   const options = {
     get(target, key) {
-      console.log('get', target, key)
-      track(target, key)
-      return target[key]
+      console.log("get", target, key);
+      track(target, key);
+      return target[key];
     },
     set(target, key, newValue) {
-      console.log('set', target, key, newValue)
-      target[key] = newValue
-      trigger(target, key)
-      return true
-    }
-  }
-  return new Proxy(obj, options)
+      console.log("set", target, key, newValue);
+      target[key] = newValue;
+      trigger(target, key);
+      return true;
+    },
+  };
+  return new Proxy(obj, options);
 }
 
 function track(target, key) {
-  let depsMap = bucket.get(target)
-  if (!depsMap) bucket.set(target, (depsMap = new Map()))
-  let deps = depsMap.get(key)
-  if (!deps) depsMap.set(key, (deps = new Set()))
-  deps.add(activeEffect)
-  activeEffect.deps.push(deps)
+  let depsMap = bucket.get(target);
+  if (!depsMap) bucket.set(target, (depsMap = new Map()));
+  let deps = depsMap.get(key);
+  if (!deps) depsMap.set(key, (deps = new Set()));
+  deps.add(activeEffect);
+  activeEffect.deps.push(deps);
 }
 
 function trigger(target, key) {
-  const depsMap = bucket.get(target)
-  const deps = depsMap.get(key)
-  if (!deps) return
-  const effectsToRun = new Set()
-  deps.forEach(dep => {
-    if (dep !== activeEffect) effectsToRun.add(dep)
-  })
-  effectsToRun.forEach(effect => effect())
+  const depsMap = bucket.get(target);
+  const deps = depsMap.get(key);
+  if (!deps) return;
+  const effectsToRun = new Set();
+  // 如果绑定的副作用不是当前执行的副作用，加入到副作用执行队列中
+  deps.forEach((dep) => {
+    if (dep !== activeEffect) effectsToRun.add(dep);
+  });
+  effectsToRun.forEach((effect) => effect());
 }
